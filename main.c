77ae376f74d5a1f70de3d1767dbdcadaa889198d
Maelle Berthet <berthet.maelle@hotmail.fr>
2022-01-11 17:56:09 +0100

    begin

---
+++ main.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mberthet <mberthet@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/11 13:20:01 by mberthet          #+#    #+#             */
+/*   Updated: 2022/01/11 17:55:28 by mberthet         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+
+int main(int argc, char **argv, char **env)
+{
+	init_struct(g_struct); /*fonction qui va initialiser notre structure globale*/
+
+
+}
