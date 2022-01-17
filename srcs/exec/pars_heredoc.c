1a22cfe9264395706046abe60d6165dcdd4ca1bf
Maelle Berthet <berthet.maelle@hotmail.fr>
2022-01-17 11:00:52 +0100

    add exec/here_doc

---
+++ srcs/exec/pars_heredoc.c
@@ -0,0 +1,11 @@
+char *heredoc_found(char *limiter, int i)
+{
+	char *name_file; //le nom du fichier qui va etre stocké dans la liste chainee d'infile
+
+	name_file = malloc(sizeof(char) * ft_strlen(ft_itoa(i)));
+	if (!name_file)
+		return (NULL);
+	name_file = ft_strdup("/tmp/tmp.txt")
+	fd_hd = open()
+
+}
